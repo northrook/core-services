79fb348ec84ce504413a59d9139ad944be44ad4f
Martin <hello@northrook.com>
2024-10-16 05:51:45 +0100

    Initial commit. WIP.

---
+++ src/CoreServicesBundle.php
@@ -0,0 +1,72 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Core;
+
+// Pathfinder cache using array-store
+// Request
+// Headers
+// Auth?
+// Settings
+use Override;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
+use function Symfony\Component\DependencyInjection\Loader\Configurator\service;
+
+/**
+ * Core shared services.
+ *
+ * - Available throughout the framework.
+ *
+ * @author Martin Nielsen
+ */
+final class CoreServicesBundle extends AbstractBundle
+{
+    #[Override]
+    public function getPath() : string
+    {
+        return \dirname( __DIR__ );
+    }
+
+    /**
+     * @param array<array-key, mixed> $config
+     * @param ContainerConfigurator   $container
+     * @param ContainerBuilder        $builder
+     *
+     * @return void
+     */
+    #[Override]
+    public function loadExtension(
+        array                 $config,
+        ContainerConfigurator $container,
+        ContainerBuilder      $builder,
+    ) : void {
+
+        $services = $container->services();
+
+        $services->defaults()
+            ->tag( 'controller.service_arguments' )
+            ->autowire();
+
+        $services
+            // Request and Response headers
+            ->set( Headers::class )
+            ->args( [
+                service( 'request_stack' ),
+                service( 'router.request_context' ),
+            ] )
+
+            // Find and return registered paths
+            ->set( Pathfinder::class )
+
+            // Current Request handler
+            ->set( Request::class )
+            ->args( [service( 'request_stack' ), service( 'http_kernel' )] )
+
+            // Settings handler
+            ->set( Settings::class );
+
+    }
+}
---
+++ src/Headers.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Core;
+
+use Symfony\Component\HttpFoundation\{HeaderBag, RequestStack, ResponseHeaderBag};
+use Symfony\Component\Routing\RequestContext;
+
+final readonly class Headers
+{
+    public HeaderBag $request;
+
+    public ResponseHeaderBag $response;
+
+    // either do public properties
+    // or use custom methods?
+
+    public function __construct(
+        RequestStack          $stack,
+        public RequestContext $requestContext,
+    ) {
+        $this->request  = $stack->getCurrentRequest()->headers;
+        $this->response = new ResponseHeaderBag();
+    }
+}
